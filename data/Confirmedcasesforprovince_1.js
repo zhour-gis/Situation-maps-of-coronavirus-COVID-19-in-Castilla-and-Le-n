4e92eb94e6c7a7ad17e9f30ab01a50e2643abc88
zhour-gis <65556461+zhour-gis@users.noreply.github.com>
2020-05-18 18:11:21 +0200

    Add files via upload

---
+++ data/Confirmedcasesforprovince_1.js
@@ -0,0 +1,16 @@
+var json_Confirmedcasesforprovince_1 = {
+"type": "FeatureCollection",
+"name": "Confirmedcasesforprovince_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "Palencia", "casos_confi": 1205.0, "fallecimien": 83.0 }, "geometry": { "type": "Point", "coordinates": [ -4.520647, 42.432128 ] } },
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "Burgos", "casos_confi": 2746.0, "fallecimien": 205.0 }, "geometry": { "type": "Point", "coordinates": [ -3.666667, 42.383333 ] } },
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "Salamanca", "casos_confi": 4152.0, "fallecimien": 358.0 }, "geometry": { "type": "Point", "coordinates": [ -6.0, 40.833333 ] } },
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "Segovia", "casos_confi": 3413.0, "fallecimien": 201.0 }, "geometry": { "type": "Point", "coordinates": [ -4.0, 41.166667 ] } },
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "Valladolid", "casos_confi": 4393.0, "fallecimien": 362.0 }, "geometry": { "type": "Point", "coordinates": [ -4.666667, 41.583333 ] } },
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "Ávila", "casos_confi": 1935.0, "fallecimien": 133.0 }, "geometry": { "type": "Point", "coordinates": [ -4.929031, 40.616092 ] } },
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "León", "casos_confi": 3569.0, "fallecimien": 407.0 }, "geometry": { "type": "Point", "coordinates": [ -6.0, 42.666667 ] } },
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "Soria", "casos_confi": 2290.0, "fallecimien": 119.0 }, "geometry": { "type": "Point", "coordinates": [ -2.666667, 41.666667 ] } },
+{ "type": "Feature", "properties": { "fecha": "2020-05-17", "provincia": "Zamora", "casos_confi": 993.0, "fallecimien": 85.0 }, "geometry": { "type": "Point", "coordinates": [ -6.0, 41.75 ] } }
+]
+}
